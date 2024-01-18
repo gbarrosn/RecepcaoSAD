7974d74debd1da103b3ab25688bbf56729aedfbd
gbarrosn <gbarros.n@gmail.com>
2024-01-18 16:29:24 -0300

    commit inicial

---
+++ src/View/LoginUser.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src.login;
+
+/**
+ *
+ * @author gbarrosn
+ */
+public class LoginUser {
+    
+    private int id_login;
+    private String login;
+    private String senha;
+    private String adm;
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the password to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the id_login
+     */
+    public int getId_login() {
+        return id_login;
+    }
+
+    /**
+     * @param id_login the id_login to set
+     */
+    public void setId_login(int id_login) {
+        this.id_login = id_login;
+    }
+
+    /**
+     * @return the adm
+     */
+    public String getAdm() {
+        return adm;
+    }
+
+    /**
+     * @param adm the adm to set
+     */
+    public void setAdm(String adm) {
+        this.adm = adm;
+    }
+    
+}
