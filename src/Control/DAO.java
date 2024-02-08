55a8b36ff160efabbc3748152d8703e2480ecfa0
gbarrosn <gbarros.n@gmail.com>
2024-02-08 13:27:08 -0300

    lendo o banco de dados para popular a tebela da interface

--- src/Control/DAO.java
+++ src/Control/DAO.java
@@ -4,18 +4,52 @@
  */
 package Control;
 import View.LoginUser;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import Model.Ramal;
 /**
  *
  * @author gbarrosn
  */
 public class DAO {
+    // Connect to the database using ConectarSQL
     
-    private LoginUser logarBanco (LoginUser login){
-    //TODO agora user a classe LoginUser para conectar no banco e ver se é adm ou usuário da recepção
+    // Retrieve a list of ramais from the table Lista_de_ramais
+    public List<Ramal> getRamais() throws ClassNotFoundException {
+        List<Ramal> ramais = new ArrayList<>();
         
-    
-    return null;
-    
-}
+        // SQL query to retrieve ramais from the table
+        String query = "SELECT ramal, nome, unidade, andar FROM Lista_de_ramais";
+        
+        ConectarSQL conectarSQL = new ConectarSQL();
+        try (Connection connection = conectarSQL.conectarPrepared();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery()) {
+            
+            // Iterate through the result set and populate Ramal objects
+            while (resultSet.next()) {
+                String ramal = resultSet.getString("ramal");
+                String nome = resultSet.getString("nome");
+                String unidade = resultSet.getString("unidade");
+                String andar = resultSet.getString("andar");
+                
+                Ramal ramalObj = new Ramal();
+                ramalObj.setGerencia(unidade);
+                ramalObj.setNome(nome);
+                ramalObj.setPavimento(andar);
+                ramalObj.setRamal(ramal);
+                ramais.add(ramalObj);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return ramais;
+    }
     
 }
---
+++ src/Model/Ramal.java
@@ -0,0 +1,40 @@
+package Model;
+
+public class Ramal {
+    private String ramal;
+    private String nome;
+    private String gerencia;
+    private String pavimento;
+
+    public void setRamal(String ramal) {
+        this.ramal = ramal;
+    }
+
+    public String getRamal() {
+        return ramal;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setGerencia(String gerencia) {
+        this.gerencia = gerencia;
+    }
+
+    public String getGerencia() {
+        return gerencia;
+    }
+
+    public void setPavimento(String pavimento) {
+        this.pavimento = pavimento;
+    }
+
+    public String getPavimento() {
+        return pavimento;
+    }
+}
