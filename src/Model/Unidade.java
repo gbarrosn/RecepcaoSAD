1ebb96f11f059c764353dbb664b54aafee466895
gbarrosn <gbarros.n@gmail.com>
2024-02-08 15:44:01 -0300

    criando model unidade

---
+++ src/Model/Unidade.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class Unidade {
+    private int id_unidade;
+    private String unidade;
+
+    public int getId_unidade() {
+        return id_unidade;
+    }
+
+    public void setId_unidade(int id_unidade) {
+        this.id_unidade = id_unidade;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+}
+    
+}
