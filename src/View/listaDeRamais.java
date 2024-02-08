bafb2881eb646340505c98bbf0190c32afdbb61a
gbarrosn <gbarros.n@gmail.com>
2024-02-08 13:54:02 -0300

    adicionando botão de voltar para tela principal

--- src/View/listaDeRamais.java
+++ src/View/listaDeRamais.java
@@ -201,6 +201,8 @@ public class listaDeRamais extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         TelaPrincipal tela = new TelaPrincipal(login);
+        tela.setVisible(true);
+        this.dispose();
         
     }//GEN-LAST:event_jButton2ActionPerformed
 
